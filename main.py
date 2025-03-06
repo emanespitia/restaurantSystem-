856d5e65a4aeb7b2d50c6f0135575f6af15eaeb1
emanespitia <eespiti@jacksonville.edu>
2025-03-06 15:03:29 -0500

    Beginning of code

---
+++ main.py
@@ -0,0 +1,10 @@
+name = input("Please enter your name: ")
+print(f"Welcome to the Online Ordering System {name}!")
+
+balance = input("please enter your account balane: ")
+
+
+print(f"Your balance is: {balance}")
+print("Please choose an item from the menu: ")
+print("1. Pizza\n2.Burger\n3.Salad\n4.Pasta\n5.Sode\n")
+
