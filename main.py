ad1946eac0c80ded8c4be0ad42e2d83266ce2def
emanespitia <eespiti@jacksonville.edu>
2025-03-11 14:47:52 -0400

    Menu update

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 name = input("Please enter your name: ")
 print(f"Welcome to the Online Ordering System {name}!")
 
-balance = input("please enter your account balance: ")
+balance = input("please enter your account balane: ")
 
+menu_items = (1, "Pizza", 15.99), (2, "Burger", 6.0), (3, "Salad", 5.0),(4, "Pasta", 7.0), (5, "Soda", 2.0)
 
 print(f"Your balance is: {balance}")
 print("Please choose an item from the menu: ")
-print("1. Pizza\n2.Burger\n3.Salad\n4.Pasta\n5.Soda\n")
+print("-" * 30)
+for number, item, price in menu_items:
+    print(f"{number}. {item:<6} Price: ${price}")
+print("-" * 30)
 
+    
